1ab1730400717f31a0a65a5c89c112c98d030d50
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-05-26 12:14:51 +0200

    installed shadcn and configured paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,17 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
 });
