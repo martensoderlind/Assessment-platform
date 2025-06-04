c7258cbb1d1f3d668c420f020f0ade9283a3ff8d
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-06-04 11:28:50 +0200

    feat: update studentMessage schema to include read status and remove classMessage table

--- backend/src/features/subjects/schema.ts
+++ backend/src/features/subjects/schema.ts
@@ -1,4 +1,10 @@
-import { pgTable, uuid, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  uuid,
+  timestamp,
+  varchar,
+  boolean,
+} from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
 import { students, subjects, teachers } from "../students/schema";
 
@@ -42,22 +48,6 @@ export const studentMessage = pgTable("student_message", {
     .references(() => students.id),
   topic: varchar().notNull(),
   message: timestamp("created_at").defaultNow(),
-  createdAt: timestamp("created_at").defaultNow(),
-});
-export const classMessage = pgTable("class_message", {
-  id: uuid("id")
-    .primaryKey()
-    .default(sql`gen_random_uuid()`),
-  subjectId: uuid("subject_id")
-    .notNull()
-    .references(() => subjects.id),
-  teacherId: uuid("teacher_id")
-    .notNull()
-    .references(() => teachers.id),
-  classId: uuid("class_id")
-    .notNull()
-    .references(() => students.id),
-  topic: varchar().notNull(),
-  message: timestamp("created_at").defaultNow(),
+  read: boolean("read").notNull().default(false),
   createdAt: timestamp("created_at").defaultNow(),
 });
